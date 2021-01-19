86e13913e830b5783583a93f8a395af17c16cc60
Rinata Baygulova <rinulya0912@gmail.com>
2021-01-19 17:42:52 +0300

    fix: поправить плейсхолдер в инпуте поисковой формы

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,7 +3,7 @@ import './SearchForm.css';
 export function SearchForm() {
   return (
     <form className="searchForm">
-      <input className="searchForm__input"></input>
+      <input className="searchForm__input" placeholder="Введите тему новости"></input>
       <button className="searchForm__button">Искать</button>
     </form>
   );
