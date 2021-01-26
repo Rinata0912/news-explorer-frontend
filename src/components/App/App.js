051b8748393458740f314809a168d655af125bf2
Rinata Baygulova <rinulya0912@gmail.com>
2021-01-26 18:39:40 +0300

    feat: добавить api

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,40 @@
 import { Main } from '../Main/Main';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, useHistory } from 'react-router-dom';
 import { SavedNews } from '../SavedNews/SavedNews';
 import { Footer } from '../Footer/Footer';
-import { useState, useCallback } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { PopupContext } from '../../contexts/PopupContext';
 import { Login } from '../Login/Login';
 import { Register } from '../Register/Register';
 import { Success } from '../Success/Success';
 import { ProtectedRoute } from '../ProtectedRoute/ProtectedRoute';
+import { api } from '../../utils/api';
 
 function App() {
-  const [isLogin, setIsLogin] = useState(true);
+  const [isLogin, setIsLogin] = useState(false);
   const [isLoginPopupOpen, setIsLoginPopupOpen] = useState(false);
   const [isRegisterPopupOpen, setIsRegisterPopupOpen] = useState(false);
   const [isSuccessPopupOpen, setIsSuccessPopupOpen] = useState(false);
+  const history = useHistory();
+
+  const handleTokenCheck = useCallback(() => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
+      api
+        .checkToken(token)
+        .then((res) => {
+          if (res) {
+            setIsLogin(true);
+            history.push('/');
+          }
+        })
+        .catch((err) => err);
+    }
+  }, [history]);
+
+  useEffect(() => {
+    handleTokenCheck();
+  }, []);
 
   const handleOpenLoginPopup = useCallback(() => {
     setIsLoginPopupOpen(true);
---
+++ src/utils/api.js
@@ -0,0 +1,121 @@
+class Api {
+  constructor(options) {
+    this._options = options;
+  }
+
+  _handleOriginalRes = (res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(res);
+  };
+
+  getToken() {
+    return localStorage.getItem('jwt');
+  }
+
+  getSavedArticles() {
+    return fetch(`${this._options.baseUrl}/articles`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.getToken()}`,
+      },
+    })
+      .then(this._handleOriginalRes)
+      .then((res) => res);
+  }
+
+  addArticle({
+    keyword,
+    title,
+    text,
+    date,
+    source,
+    link,
+    image,
+  }) {
+    return fetch(`${this._options.baseUrl}/articles`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.getToken()}`,
+      },
+      body: JSON.stringify({
+        keyword,
+        title,
+        text,
+        date,
+        source,
+        link,
+        image,
+      }),
+    })
+      .then(this._handleOriginalRes)
+      .then((res) => res);
+  }
+
+  removeArticle({ id }) {
+    return fetch(`${this._options.baseUrl}/articles/${id}`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.getToken()}`,
+      },
+    })
+      .then(this._handleOriginalRes)
+      .then((res) => res);
+  }
+
+  getUserInfo() {
+    return fetch(`${this._options.baseUrl}/users/me`, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${this.getToken()}`,
+      },
+    })
+      .then(this._handleOriginalRes)
+      .then((res) => res);
+  }
+
+  signUp({ name, password, email }) {
+    return fetch(`${this._options.baseUrl}/signup`, {
+      method: 'POST',
+      headers: this._options.headers,
+      body: JSON.stringify({
+        name,
+        password,
+        email,
+      }),
+    })
+      .then(this._handleOriginalRes)
+      .then((res) => res);
+  }
+
+  signIn({ email, password }) {
+    return fetch(`${this._options.baseUrl}/signin`, {
+      method: 'POST',
+      headers: this._options.headers,
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    })
+      .then(this._handleOriginalRes)
+      .then((res) => res);
+  }
+}
+
+const api = new Api({
+  // baseUrl: `${window.location.protocol}//api.whatsthenews.students.nomoredomains.work`,
+  baseUrl: '/api',
+  headers: {
+    'Content-Type': 'application/json',
+    Accept: 'application/json',
+  },
+})
+
+export { api };
