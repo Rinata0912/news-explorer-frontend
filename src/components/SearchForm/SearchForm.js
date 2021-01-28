b2feff0691748786f9168ef15212fbc54638f4f2
Rinata Baygulova <rinulya0912@gmail.com>
2021-01-29 01:37:55 +0300

    feat: добавить валидацию при поиске статей

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,16 +1,28 @@
 import { Button } from '../Button/Button';
 import './SearchForm.css';
 import { useForm } from '../../utils/useForm';
+import { useState } from 'react';
 
 export function SearchForm({ onSearch }) {
   const { formValues, handleChange, setFormValues } = useForm({
     keyword: '',
   });
+  const [isEmptyInput, setIsEmptyInput] = useState(false);
+  // const { formValues, handleChange, setFormValues } = useFormValidation({
+  //   keyword: '',
+  // });
 
   const handleSubmit = (evt) => {
     evt.preventDefault();
 
-    onSearch(formValues.keyword);
+    const keyword = formValues.keyword.trim();
+
+    if(keyword === '') {
+      setIsEmptyInput(true);
+      return;
+    }
+
+    onSearch(keyword);
 
     setFormValues({
       ...formValues,
@@ -18,11 +30,16 @@ export function SearchForm({ onSearch }) {
     });
   };
 
+  const handleInputChange = (evt) => {
+    setIsEmptyInput(false);
+    handleChange(evt);
+  };
+
   return (
     <form onSubmit={handleSubmit} className="searchForm">
-      <input onChange={handleChange} value={formValues.keyword} className="searchForm__input" name="keyword" placeholder="Введите тему новости"></input>
-      {/* <button className="searchForm__button">Искать</button> */}
+      <input onChange={handleInputChange} value={formValues.keyword} className="searchForm__input" name="keyword" placeholder="Введите тему новости"></input>
       <Button theme="form">Искать</Button>
+      {isEmptyInput && <span className="searchForm__error">Нужно ввести ключевое слово</span>}
     </form>
   );
 }
